29686d3e0bdf8f5453e632a1582b757d33b2bdcf
Taritsyn <taritsyn@gmail.com>
2022-01-18 17:22:04 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ClearScript.V8;
+
+using (var engine = new V8ScriptEngine())
+{
+    Console.WriteLine("Start script execution...");
+
+    engine.Execute(@"var count = 0;
+
+for (let i = 1; i < 1000000; i++) {
+    count++;
+}"
+    );
+    var count = (int)engine.Evaluate("count");
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Number of iterations: {0}", count);
+    Console.ForegroundColor = ConsoleColor.White;
+}
